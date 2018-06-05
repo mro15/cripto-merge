07e93117ea4712767cfe9dd6c642f23d787ff669
Marcela Ribeiro de Oliveira <mro15@inf.ufpr.br>
2018-06-05 19:41:28 -0300

    definindo uns todo

--- criptomerge.py
+++ criptomerge.py
@@ -10,6 +10,7 @@ import re
 def read_args():
 	parser = argparse.ArgumentParser(description='Os parametros sao:')
 	parser.add_argument('-f', '--file', type=str, required=True, help='nome do arquivo a ser criptografado')
+	parser.add_argument('-k', '--key', type=str, required=True, help='nome do arquivo com a chave')
 	return parser.parse_args()
 
 def open_file(file_in):
@@ -32,6 +33,10 @@ def file_parser(f):
 	print b
 	return b
 
+#read key and mount the estencil
+def read_key(key_file):
+	pass
+
 def read_blocks(b):
 	lol = []
 	cont = 0
@@ -39,16 +44,19 @@ def read_blocks(b):
 		for j in range(0, 64):
 			lol.append(b[cont])
 			cont +=1
-		convert_to_matrix(lol)
+		block = convert_to_matrix(lol)
 		lol = []
 	m = len(b)%64
 	if(m!=0):
 		for i in range(0, m):
 			lol.append(b[cont])
 			cont+=1
-		convert_to_matrix(lol)
+		block = convert_to_matrix(lol)
 	print lol
 
+#a ideia dessa funcao e receber a matriz e a key e fazer o estencil
+def mult_by_key(block, key):
+	pass
 
 def convert_to_matrix(b):
 	size = (8, 8)
@@ -78,5 +86,9 @@ def convert_to_matrix(b):
 if __name__ == "__main__":
 	args = read_args()
 	f = open_file(args.file)
+	k = open_file(args.key)
+	key = read_key(k)
 	b = file_parser(f)
 	read_blocks(b)
+	#TODO: as imagens resultantes serao escritas em um diretorio
+	#			como serao os nomes dos arquivos
