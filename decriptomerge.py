ab6876b2802027dcebcad703850b5b4e54be9f8b
Marcela Ribeiro de Oliveira <mro15@inf.ufpr.br>
2018-06-08 15:05:36 -0300

    programa pra descriptografar

--- decriptomerge.py
+++ decriptomerge.py
@@ -23,9 +23,36 @@ def open_file(file_in):
 	f = open(file_in, 'r')
 	return f
 
+#read key and mount the estencil
+def read_key(key_file):
+	key = []
+	for line in key_file:
+			key.append(map(int, line.strip().split(' ')))
+	return np.array(key)
+
+def decript(img, key, f):
+	blocks = len(img)/8
+	ik, jk = key.shape[:2]
+	for b in range(0, blocks):
+		dtext= np.zeros((8, 8), dtype=np.int)
+		offset = b*8
+		for i in range(0, 8):
+			for j in range(0, 8):
+				dtext[i][j] = ((img[offset+i][j][1]*255)+img[offset+i][j][2])/(key[i%ik][j%jk])
+		dec_to_text(dtext, f)
+
+def dec_to_text(block, f):
+	text = ""
+	for i in range(0, 8):
+		for j in range(0, 8):
+			text = text + chr(block[i][j])
+	f.write(text)
+
 
 if __name__ == "__main__":
 	args = read_args()
+	f = open(args.file, 'w')
 	k = open_file(args.key)
+	key = read_key(k)
 	img = cv2.imread(args.image)
-	print img
+	decript(img, key, f)
